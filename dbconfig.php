b0270b1378e28004fdc28cd84c97c83d491e9a4a
MurphCCC <mkcnrd@gmail.com>
2017-05-16 15:01:37 -0400

    Moved db credentials to seperate untracked file

---
+++ dbconfig.php
@@ -0,0 +1,5 @@
+<?php
+$hostname='localhost';
+$username='root';
+$password='Tq5bZNwOHzi8';
+?>
--- functions.php
+++ functions.php
@@ -2,11 +2,11 @@
 
 // Scan a directory containing images and spit out content for each image.
 function image_gallery() {
-    $i = 0; 
+    $i = 0;
     $dir = 'images/gallery/thumbs/';
     if ($handle = opendir($dir)) {
         while (($file = readdir($handle)) !== false){
-            if (!in_array($file, array('.', '..')) && !is_dir($dir.$file)) 
+            if (!in_array($file, array('.', '..')) && !is_dir($dir.$file))
             	echo '
 						<article>
 							<a href="images/gallery/fulls/'.$file.'" class="image">
@@ -43,14 +43,14 @@ function contact() {
         if(isset($_POST["submit"])){
         $hostname='localhost';
         $username='root';
-        $password='password';
+        $password='Tq5bZNwOHzi8';
 
             try {
 
-            $dbh = new PDO("mysql:host=$hostname;dbname=wedding",$username,$password);
+            $dbh = new PDO("mysql:host=$hostname;dbname=the_murphs",$username,$password);
             $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // <== add this line
-            $sql = "INSERT INTO sign_up_form (name, email, message, client_ip, time_stamp)
-            VALUES ('".$_POST["name"]."','".$_POST["email"]."','".$_POST["message"]."','".$_SERVER['REMOTE_ADDR']."', NOW())";
+            $sql = "INSERT INTO sign_up_form (name, email, guests, message, client_ip, time_stamp)
+            VALUES ('".$_POST["name"]."','".$_POST["email"]."','".$_POST["guests"]."','".$_POST["message"]."','".$_SERVER['REMOTE_ADDR']."', NOW())";
 
             if ($dbh->query($sql)) {
                 echo "<script type= 'text/javascript'>alert('Success');</script>";
